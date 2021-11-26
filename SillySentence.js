86ae24b632ba8ab1fca4aa68f1c1fbf78c9016d6
Dajocoh <danieljcoh@gmail.com>
2021-11-26 00:15:59 -0500

    js exercise (sillySentence) (Basically madlibs)

---
+++ SillySentence.js
@@ -0,0 +1,17 @@
+    //PROBLEM: 
+/*
+    Write a function that takes in three string parameters and returns them in the blanks in the following sentence.
+*/
+
+
+// Write your function here:
+const sillySentence = (adjective, verb, noun) => {
+    return `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`; 
+}
+
+  // Uncomment the line below when you're ready to try out your function
+console.log(sillySentence('excited', 'love', 'functions')); 
+console.log(sillySentence('hairy', 'run', 'chicken')); 
+console.log(sillySentence('grouchy', 'spin', 'mountains')); 
+
+  // We encourage you to add more function calls of your own to test your code!
